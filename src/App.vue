88131e9fb18c61e8e6fb87d728c996cd21eca3a1
NeaCS <naldi.castro@gmail.com>
2022-04-06 22:11:00 -0400

    uploading

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div class="contenedor">
+      <h1>Crear nueva tarea</h1>
+      <label>
+        Ingresar nueva tarea
+        <input type="text" v-model="tarea" />
+      </label>
+      <button @click="addTarea" type="button" class="btn btn-success btn-sm">
+        Agregar
+      </button>
+      <ul v-for="(tarea, index) in tareas" :key="index" Class="list-group">
+        <li class="list-group-item">{{ index + 1 }} - {{ tarea }}</li>
+      </ul>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: "App",
+  data() {
+    return {
+      tarea: "",
+      tareas: [],
+    };
+  },
+  methods: {
+    addTarea() {
+      const { tarea } = this;
+      this.tareas.push(tarea);
+      this.tarea = "";
+    },
+  },
+};
+</script>
+
+<style>
+.btn {
+  margin-left: 1rem;
+  margin-bottom: 0.3rem;
+}
+
+.contenedor {
+  text-align: center;
+}
+
+.list-group-item {
+  align-self: center;
+  width: 40%;
+}
+</style>
